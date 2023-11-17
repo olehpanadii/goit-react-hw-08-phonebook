346e39e981b46c799641a3de68e19292179f4fd1
olehpanadii <92667024+olehpanadii@users.noreply.github.com>
2023-11-17 21:48:36 +0000

    Deploying to gh-pages from @ olehpanadii/goit-react-hw-08-phonebook@9244fa8d1505ef9e1cd145a3390b6fd0856538a0 🚀

--- static/js/680.7e7d723d.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[680],{680:function(e,r,n){n.r(r),n.d(r,{default:function(){return t}});var a=n(184);function t(){return(0,a.jsx)("div",{children:(0,a.jsxs)("h1",{children:["Task manager welcome page"," ",(0,a.jsx)("span",{role:"img","aria-label":"Greeting icon",children:"\ud83d\udc81\u200d\u2640\ufe0f"})]})})}}}]);
-//# sourceMappingURL=680.7e7d723d.chunk.js.map
